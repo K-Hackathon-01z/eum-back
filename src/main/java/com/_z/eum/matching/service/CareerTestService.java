62fbe2a78fa2b7d3d443e60a769b6f440002bd74
vvineey <vvin506@naver.com>
2025-08-04 23:06:29 +0900

    update : 기술 매칭 테스트 결과 저장 후 불러오는 로직으로 변경

--- src/main/java/com/_z/eum/matching/service/CareerTestService.java
+++ src/main/java/com/_z/eum/matching/service/CareerTestService.java
@@ -2,22 +2,47 @@ package com._z.eum.matching.service;
 
 import com._z.eum.matching.dto.Response.QuestionResponse;
 import com._z.eum.matching.entity.CareerTestQuestion;
+import com._z.eum.matching.entity.CareerTestRecommendation;
+import com._z.eum.matching.entity.CareerTestResult;
+import com._z.eum.matching.repository.CareerTestRecommendationRepository;
 import com._z.eum.matching.repository.CareerTestRepository;
-import com._z.eum.matching.repository.CareerTestResultRepository;
+import com._z.eum.matching.repository.CareerTestOptionSkillRepository;
 import com._z.eum.matching.dto.Response.MatchResult;
+import com._z.eum.matching.repository.CareerTestResultRepository;
+import com._z.eum.skill.entity.SkillCategory;
+import com._z.eum.skill.repository.SkillRepository;
 import org.springframework.stereotype.Service;
 
+import javax.swing.*;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CareerTestService {
 
     private final CareerTestRepository careerTestRepository;
+    private final CareerTestOptionSkillRepository careerTestOptionSkillRepository;
+    private final CareerTestRecommendationRepository careerTestRecommendationRepository;
     private final CareerTestResultRepository careerTestResultRepository;
+    private final SkillRepository skillRepository;
 
-    public CareerTestService(CareerTestRepository careerTestRepository, CareerTestResultRepository careerTestResultRepository) {
-        this.careerTestResultRepository = careerTestResultRepository;
+    public CareerTestService(CareerTestRepository careerTestRepository,
+                             CareerTestOptionSkillRepository careerTestOptionSkillRepository,
+                             CareerTestRecommendationRepository careerTestRecommendationRepository,
+                             CareerTestResultRepository careerTestResultRepository,
+                             SkillRepository skillRepository) {
+
+        this.careerTestOptionSkillRepository = careerTestOptionSkillRepository;
         this.careerTestRepository = careerTestRepository;
+        this.careerTestRecommendationRepository = careerTestRecommendationRepository;
+        this.careerTestResultRepository = careerTestResultRepository;
+        this.skillRepository = skillRepository;
+    }
+
+    //검사 이력 조회
+    public boolean hasTakenTest(Integer userId) {
+        return careerTestResultRepository.existsByUserId(userId);
     }
 
 
@@ -45,10 +70,27 @@ public class CareerTestService {
                 .toList();
     }
 
-    public List<MatchResult> calculateTopSkills(List<Integer> optionIds) {
-        return careerTestResultRepository.findSkillScoresByOptionIds(optionIds)
-                .stream()
-                .limit(10)
+    public List<MatchResult> processCareerTest(Integer userId, List<Integer> optionIds) {
+        List<MatchResult> results = careerTestOptionSkillRepository.findSkillScoresByOptionIds(optionIds)
+                .stream().limit(10).toList();
+
+        CareerTestResult result = careerTestResultRepository.save(new CareerTestResult(userId));
+
+        int rank = 1;
+        for (MatchResult r : results) {
+            careerTestRecommendationRepository.save(new CareerTestRecommendation(result, r.skillId(), (int) r.totalScore(), rank++));
+        }
+
+        return results;
+    }
+
+    public List<MatchResult> getSavedRecommendations(Integer userId) {
+        CareerTestResult latestResult = careerTestResultRepository.findTopByUserIdOrderByUpdatedAtDesc(userId)
+                .orElseThrow(() -> new NoSuchElementException("검사 결과가 없습니다"));
+
+        return latestResult.getRecommendations().stream()
+                .sorted(Comparator.comparingInt(CareerTestRecommendation::getRanking))
+                .map(r -> new MatchResult(r.getSkillId(), skillRepository.findById(r.getSkillId()).orElseThrow().getName(),r.getScore()))
                 .toList();
     }
 }

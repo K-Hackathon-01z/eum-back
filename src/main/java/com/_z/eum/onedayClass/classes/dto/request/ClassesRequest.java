936adf601720c999be8a6d3178d6d16c47900d76
vvineey <vvin506@naver.com>
2025-09-28 21:25:45 +0900

    update : dto에서 관심 인원 수 제거

--- src/main/java/com/_z/eum/onedayClass/classes/dto/request/ClassesRequest.java
+++ src/main/java/com/_z/eum/onedayClass/classes/dto/request/ClassesRequest.java
@@ -4,13 +4,28 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "클래스 생성, 수정 요청 DTO")
 public record ClassesRequest(
+
+        @Schema(description = "기술 카테고리", example = "1")
         int skillId,
+
+        @Schema(description = "기술 장인", example = "1")
         int artisanId,
+
+        @Schema(description = "클래스 이름", example = "목공 기초 — 선반 만들기")
         String title,
+
+        @Schema(description = "클래스 사진", example = "example.com")
         String photoUrl,
+
+        @Schema(description = "클래스 설명", example = "목공 기초 — 선반 만들기는 목공 기초부터 시작하여 선반을 만드는 아주아주 재밌는 원데이 클래스입니다.")
         String description,
+
+        @Schema(description = "클래스 가격", example = "56,000")
         int price,
+
+        @Schema(description = "클래스 위치", example = "서울특별시 광진구")
         String location,
-        int capacity,
-        int interestedCount
+
+        @Schema(description = "클래스 최대 수용 인원", example = "12")
+        int capacity
 ) {}

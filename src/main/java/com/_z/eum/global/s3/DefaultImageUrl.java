6e8f3fd4808b1f60e8cee398a0988e61c2c31886
vvineey <vvin506@naver.com>
2025-10-08 22:27:01 +0900

    feat : defaultImageURL 상수 파일로 처리

---
+++ src/main/java/com/_z/eum/global/s3/DefaultImageUrl.java
@@ -0,0 +1,8 @@
+
+package com._z.eum.global.s3;
+
+public class DefaultImageUrl {
+    public static final String ARTISAN = "https://eum-01z-bucket.s3.ap-southeast-2.amazonaws.com/artisans/default_profile.jpg";
+    public static final String CLASS = "https://eum-01z-bucket.s3.ap-southeast-2.amazonaws.com/classes/default_class.jpg";
+    public static final String SKILL = "https://eum-01z-bucket.s3.ap-southeast-2.amazonaws.com/skills/default_skill.jpg";
+}
